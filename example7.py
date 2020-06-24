ee315f15a5af18b34de3eafa93ef3ada55637419
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-06-24 17:51:16 +0700

    while break true

--- example7.py
+++ example7.py
@@ -1,13 +1,18 @@
 with open('example7.txt') as text:
-    dish = {}
-    e = []
-    a = text.readline().rstrip()
-    b = int(text.readline().rstrip())
-    c = ['ingredient_name', 'quantity', 'measure']
+    while True:
+        dish = {}
+        dishes = {}
+        e = []
+        a = text.readline().rstrip()
+        if not a:
+            break
+        b = int(text.readline().rstrip())
+        c = ['ingredient_name', 'quantity', 'measure']
+        for i in range(b):
+            d = text.readline().rstrip().split(' | ')
+            for f in d:
+                d[1] = int(d[1])
+            e.append(dict(zip(c, d)))
+        print(dish.fromkeys((a,), e))
+        text.readline()
     
-    for i in range(b):
-        d = text.readline().rstrip().split(' | ')
-        for f in d:
-            d[1] = int(d[1])
-        e.append(dict(zip(c, d)))
-    print(dish.fromkeys((a,), e))
