ffba39256efdd253555164a301d616abd2ab728d
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-06-25 22:33:39 +0700

    work+

--- example7.py
+++ example7.py
@@ -1,5 +1,5 @@
 def make_dict():
-    with open('example7.txt') as text:
+    with open('recipes.txt') as text:
         dish = {}
         dishes_dict = {}
         while True:
@@ -16,53 +16,48 @@ def make_dict():
                 e.append(dict(zip(c, d)))
                 dishes_dict.update(dish.fromkeys((a,), e))
             text.readline()
-##        print(dishes_dict )
     return dishes_dict
 
 make_dict()
 
-##def get_shop_list_by_dishes(dishes, person_count):
-person_count = int(input('Введите количество гостей'))
-dishes = input( 'Введите блюда через запятую' ).split(', ')
-##print(dishes)
+def get_shop_list_by_dishes(dishes, person_count):
 
-k = []
-kk = []
-l = ['measure', 'quantity']
-p = []
-n = {}
-o = {}
-t = {}
-s = {}
-for dish in dishes:
-
-    if dish in make_dict().keys():
-        for ingredient in make_dict().get(dish):
-##            print(ingredient)
-            m = []
-            k = ingredient.get('ingredient_name')
-##            print('k:', k)
-            kk.append(k)
-##            print('kk:', kk)
-            m = (ingredient.get('measure'), ingredient.get('quantity') * person_count)
-##            print('m:', m)
-            n = dict(zip(l,m))
-##            print('n:', n)
-            if kk.count(k) == 1:
-                print('kk_count:', kk.count(k))
-                t.update(o.fromkeys((k,), n))
     
-            else:
-                print('kk_count:', kk.count(k))
-##                (t.pop(k).get('quantity'))     уже умножено на число гостей
-                print(ingredient.get('quantity'))
-##              
-                m = (ingredient.get('measure'), ingredient.get('quantity')*person_count + (t.pop(k).get('quantity')))
-                print('m:', m)
+##person_count = int(input('Введите количество гостей'))
+##dishes = input( 'Введите блюда через запятую' ).split(', ')
+
+    units = ['measure', 'quantity']
+    current_ingredient = 0
+    ingredients_list = []
+    how_much_units = {}
+    one_ingredient_dict = {}
+    shop_dict = {}
+    for dish in dishes:
+        if dish in make_dict().keys():
+            for ingredient in make_dict().get(dish):
+                ingredients_for_all_person = []
+                current_ingredient = ingredient.get('ingredient_name')
+                ingredients_list.append(current_ingredient)
+                ingredients_for_all_person = (ingredient.get('measure'),
+                ingredient.get('quantity') * person_count)
+                how_much_units = dict(zip(units, ingredients_for_all_person))
+                        
+                if ingredients_list.count(current_ingredient) == 1:
+                    shop_dict.update(one_ingredient_dict.fromkeys(
+                (   current_ingredient,), how_much_units))
                 
-                n = dict(zip(l,m))
-                t.update(o.fromkeys((k,), n))
-print('t:', t)
+                else:
+                    ingredients_for_all_person = (ingredient.get('measure'),
+                    ingredient.get('quantity')*person_count +
+                    (shop_dict.pop(current_ingredient).get('quantity')))
+                
+                    how_much_units = dict(zip(units, ingredients_for_all_person))
+                    shop_dict.update(one_ingredient_dict.fromkeys(
+                    (current_ingredient,), how_much_units))
+    print(shop_dict)
+    return
+get_shop_list_by_dishes(['Омлет', 'Салат'], 2)
+
    
 
   
